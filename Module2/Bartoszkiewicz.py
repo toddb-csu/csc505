5e0215b41f7a67f47c1ed21bb06803270649c54e
Todd Bartoszkiewicz <todd.bart@gmail.com>
2025-02-24 00:09:28 -0500

    Module 2 Critical Thinking

--- Module2/Bartoszkiewicz.py
+++ Module2/Bartoszkiewicz.py
@@ -7,6 +7,7 @@
 # elements of the diagram and return these objects
 # in a well-formatted output.
 class Bartoszkiewicz:
+    # Default model stored in the dictionary data type
     my_dict = {
         "Communication": ["project initiation", " initial high-level requirements gathering",
                           "document initial requirements", "identify key stakeholders"],
@@ -19,35 +20,44 @@ class Bartoszkiewicz:
         "Deployment": ["delivery","maintenance & support", "feedback", "TAM"]
     }
 
+    # Print the phase with the steps in a nice box
     def print_box(self, text, index, items):
+        # Find the max width between the phase and all the steps
         width = len(text)
         for item in items:
             if len(item) > width:
                 width = len(item)
+        # Print the Phase in a pretty box
         print(index * "   " + "{}".format("| " if index > 0 else "") + "+" + "-" * width + "+")
         print(index * "   " + "{}".format("->" if index > 0 else "") + "|" + text.center(width) + "|")
         print(index * "   " + "{}".format("  " if index > 0 else "") + "+" + "-" * width + "+")
+        # Print all of the steps in the Phase
         for item in items:
             print(index * "   " + "{}".format("  " if index > 0 else "") + "|" + item.center(width) + "|")
+        # Only print division lines if there are steps
         if len(items) > 0:
             print(index * "   " + "{}".format("  " if index > 0 else "") + "+" + "-" * width + "+")
 
+    # Print only the Phase headings in a pretty box
     def print_headings(self, text, index):
         width = len(text)
         print(index * "   " + "{}".format("| " if index > 0 else "") + "+" + "-" * width + "+")
         print(index * "   " + "{}".format("->" if index > 0 else "") + "|" + text.center(width) + "|")
         print(index * "   " + "{}".format("  " if index > 0 else "") + "+" + "-" * width + "+")
 
+    # Loop through the default model and only print the phase headings
     def display_model_headings(self):
         for key in self.my_dict:
             index = list(self.my_dict).index(key)
             self.print_headings(key, index)
 
+    # Loop through the default model and print the phase headings and the steps
     def display_model(self):
         for key in self.my_dict:
             index = list(self.my_dict).index(key)
             self.print_box(key, index, self.my_dict[key])
 
+    # Create a custom user model
     def get_user_model(self):
         user_dict = dict()
         phase_choice = input("Do you want to enter a Phase for your Model? (yes/no): ").lower()
@@ -66,18 +76,7 @@ class Bartoszkiewicz:
 
 if __name__ == "__main__":
     print("Welcome to the Bartoszkiewicz Model Creator!")
-    # communication = input("Enter details for Communication: ")
-    # planning = input("Enter details for Planning: ")
-    # modeling = input("Enter details for Modeling: ")
-    # construction = input("Enter details for Construction: ")
-    # development = input("Enter details for Development: ")
-    # feedback_loop = input("Enter details for Feedback Loop: ")
-    # incremental_delivery = input("Enter details for Incremental Delivery: ")
-    # customer_involvement = input("Enter details for Customer Involvement: ")
-    # continuous_testing = input("Enter details for Continuous Testing: ")
-
     model = Bartoszkiewicz()
-
     default_model_choice = input("Do you want to print the default Bartoszkiewicz Model? (yes/no): ").lower()
     if default_model_choice == "yes":
         phase_headings_choice = input("Do you want to only print the Phase headings? (yes/no): ").lower()
